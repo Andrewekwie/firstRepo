03347b276f1a55b02cb042e084a1ceadf1135f4f
andrewekiwe <andrewekiwe@gamil.com>
2024-10-25 17:07:06 +0300

    hello Карлинский Владислав

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print("hello world")
-fjdslmknask="test"
+import datetime
+date = datetime.date.today().year
+month = datetime.date.today().month
+day = datetime.date.today().day
+ma = date - 2012
+
+if month == 12 and day > 15:
+        plusorminus = 0
+else:
+    plusorminus = 1
+
+if
+
+print(plusorminus)
+print(month)
+print(date)
+print()
+a = input("your age - ")
+n = input("your name - ")
+
+if a < 11:
+    yo = a - 11
+
+print("hello",n, "i`m")
