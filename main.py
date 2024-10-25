be2cd2e13cf43ec97e1fb1873b250a81c03befaf
andrewekiwe <andrewekiwe@gamil.com>
2024-10-25 17:29:37 +0300

    its a final brain cell

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ date = datetime.date.today().year
 month = datetime.date.today().month
 day = datetime.date.today().day
 ma = date - 2012
-
+text = "youre age exacly like mine"
 if month == 12 and day > 15:
         plusorminus = 0
 else:
     plusorminus = 1
-
-if
-
+if plusorminus == 1:
+    ma = date - 2013
+else:
+    ma = date - 2012
 print(plusorminus)
 print(month)
 print(date)
-print()
-a = input("your age - ")
+print(ma)
+a = int(input("your age - "))
 n = input("your name - ")
-
-if a < 11:
-    yo = a - 11
-
-print("hello",n, "i`m")
+if a > ma:
+    yooy = a - ma
+    text = "you are",yooy,"year older then me"
+if a < ma:
+    yooy = ma - a
+    text = " you are", yooy, "younger then me "
+pt = text
+print("hello",n, text,"your age",a )
