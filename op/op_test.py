a030b06c588d73d0530d412b412f5439d1489c68
ps21 <andrewekiwe@gmail.com>
2024-11-24 09:50:20 +0200

    clas work

---
+++ op/op_test.py
@@ -0,0 +1,24 @@
+# 1 = 33.8
+
+corf = input("C or F - ")
+
+class temperature():
+    def __init__(self, Celsius: float,Fahrenheit: float):
+        self.Celsius = Celsius
+        self.Fahrenheit = Fahrenheit
+
+
+    if corf == "C" or corf == "c":
+        def celsius_to_fahrenheit(Celsius):
+            print(Celsius * 33.8,"F")
+    celsius_to_fahrenheit(Celsius=temperature.Celsius)
+
+
+
+    def fahrenheit_to_celsius(self):
+        pass
+
+
+    def set_temperature(self, temperature: float):
+        pass
+temperature(1,20)
