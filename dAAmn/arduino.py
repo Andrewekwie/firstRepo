22b62dd07d54baf98d4e3c4c8181ff02c426eba6
andrewekiwe <andrewekiwe@gamil.com>
2024-12-02 23:13:12 +0200

    0

--- dAAmn/arduino.py
+++ dAAmn/arduino.py
@@ -1,28 +1,56 @@
 import pyfirmata2
 import time
+import tkinter as tk
+
+
+
+
+
+
+
+
+
+
 try:
     PORT = pyfirmata2.Arduino.AUTODETECT
 except:
     print("conetc your arduino bozzo")
 
-answer = input("T or F ")
 
 
 # Creates a new board
 board = pyfirmata2.Arduino(PORT)
 pwm_5 = board.get_pin('d:5:p')
 pwm_5.write(0)
-if answer == "T":
-    while True:
-        pwm_5.write(0)
-        time.sleep(0.2)
-        pwm_5.write(1)
-        time.sleep(0.2)
-while True:
+
+
+def on_button1_click():
     try:
-        pwm_5.write(int(input("SPEEEEED or not? (1-128)")))
+        pwm_5.write(0)
     except:
-            try:
-                pwm_5.write(int(input("SPEEEEED or not? (1-128)")))
-            except:
-                print("stoopid")
+        print("Eroor")
+    print(pwm_5)
+
+def on_button2_click():
+    try:
+        pwm_5.write(1)
+    except:
+        print("Eroor")
+    print(pwm_5)
+
+
+# Создаем главное окно
+root = tk.Tk()
+root.title("Пример с двумя кнопками")
+root.geometry("300x150")
+
+# Создаем первую кнопку
+button1 = tk.Button(root, text="-10",command=on_button1_click)
+button1.pack(pady=10)
+
+# Создаем вторую кнопку
+button2 = tk.Button(root, text="10", command=on_button2_click)
+button2.pack(pady=10)
+
+# Запускаем главный цикл обработки событий
+root.mainloop()
